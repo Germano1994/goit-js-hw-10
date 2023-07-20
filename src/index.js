87cf391dccfe89329e0a4cb9351db27639de7dd4
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-07-20 15:58:40 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const catInfo = document.querySelector('.cat-info');
 axios.defaults.headers.common['x-api-key'] =
   'live_bbSmbWd4LO9F9gCofL8a7JnNnUrFCOQWnS7je4DpFmM1scOOeEW58Q1N4NYsjYvB';
 
+
 function showLoader() {
   loader.style.display = 'block';
 }
@@ -41,8 +42,78 @@ breedSelect.addEventListener('input', async (event) => {
 
   
   fetchTimeout = setTimeout(async () => {
-    await fetchCatByBreed(breedId);
+    await getCatByBreed(breedId);
   }, 500);
 });
 
-fetchBreeds();
+(async() =>{
+  await getBreeds();
+})();
+
+
+
+
+async function getCatByBreed(breedId) {
+  showLoader();
+  hideError();
+
+  try {
+  
+    const catData = await fetchCatByBreed(breedId);
+
+    if (!catData) {
+      clearCatInfo();
+    } else {
+      catInfo.innerHTML = `
+        <img src="${catData.url}" alt="Cat Image" class="cat-image">
+        <p><strong>Breed:</strong> ${catData.breeds[0].name}</p>
+        <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
+        <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
+      `;
+
+      catInfo.style.display = 'block';
+    }
+  } catch (error) {
+    showError();
+    clearCatInfo(); 
+  }
+
+  hideLoader();
+}
+
+
+async function getBreeds() {
+  showLoader();
+  hideError();
+console.log()
+  try {
+    const loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.style.display = 'block';
+
+    
+    const breeds = await fetchBreeds();
+
+    if (!breeds || breeds.length === 0) {
+      breedSelect.innerHTML = '';
+      showError();
+      return;
+    }
+
+    const catsInfo = breeds
+      .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+      .join('');
+    breedSelect.innerHTML = catsInfo;
+
+    breedSelect.style.display = 'block';
+
+    const firstBreedId = breeds[0].id;
+    await fetchCatByBreed(firstBreedId);
+  } catch (error) {
+    showError();
+  } finally {
+    const loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.style.display = 'none';
+  }
+
+  hideLoader();
+}
